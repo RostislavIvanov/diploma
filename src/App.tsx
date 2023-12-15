8038f0f6ad842a47364bbb0dbdd49c0c3fb6f935
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2023-12-15 19:57:22 +0300

    Добавление функционала размещения и движения блоков

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
+import { useState } from "react";
 import Block from "./components/Block/Block.tsx";
 import Arrow from "./components/Arrow/Arrow.tsx";
-import './App.css'
+import './App.css';
+
 const App = () => {
+    const [blocks, setBlocks] = useState([
+        { id: 1, x: 50, y: 50, content: 'Изображение' },
+        { id: 2, x: 50, y: 150, content: 'Экспозиция +10' },
+        { id: 3, x: 300, y: 80, content: 'Светлое изображение', className: 'big' }
+    ]);
+
+    const addBlock = () => {
+        const newBlock = {
+            id: Math.random(), // Генерируем случайный ID (можно использовать UUID для более уникальных значений)
+            x: 50,
+            y: 250, // Задайте начальное положение нового блока
+            content: 'Новый блок' // Задайте содержимое нового блока
+        };
+
+        setBlocks([...blocks, newBlock]);
+    };
+
+    const handleBlockMove = (id: number, newX: number, newY: number) => {
+        const updatedBlocks = blocks.map(block =>
+            block.id === id ? { ...block, x: newX, y: newY } : block
+        );
+        setBlocks(updatedBlocks);
+    };
+
     return (
         <div style={{ position: 'relative', width: '100%', height: '100vh', border: '1px solid #ccc' }}>
-            <Block x={50} y={50}>Изображение</Block>
-            <Block x={50} y={150}>Экспозиция +10</Block>
+            {blocks.map(block => (
+                <Block
+                    key={block.id}
+                    x={block.x}
+                    y={block.y}
+                    className={block.className}
+                    onMouseMove={(newX, newY) => handleBlockMove(block.id, newX, newY)}
+                >
+                    {block.content}
+                </Block>
+            ))}
             <Arrow startX={183} startY={80} endX={308} endY={120} />
             <Arrow startX={195} startY={148} endX={308} endY={110} />
-            <Block x={300} y={80} className={'big'}>Светлое изображение</Block>
+            <button onClick={addBlock}>Добавить блок</button>
         </div>
     );
 };
--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,14 +1,44 @@
-import { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren, useState } from "react";
 import './Block.css';
+
 interface IBlockProps extends PropsWithChildren {
     x: number;
     y: number;
     className?: string;
+    onMouseMove?: (newX: number, newY: number) => void;
 }
 
-const Block: FC<IBlockProps> = ({ x, y, children, className }) => {
+const Block: FC<IBlockProps> = ({ x, y, children, className, onMouseMove }) => {
+    const [isDragging, setIsDragging] = useState(false);
+    const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        setIsDragging(true);
+        const offsetX = event.clientX - x;
+        const offsetY = event.clientY - y;
+        setDragOffset({ x: offsetX, y: offsetY });
+    };
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (isDragging) {
+            const newX = event.clientX - dragOffset.x;
+            const newY = event.clientY - dragOffset.y;
+            onMouseMove && onMouseMove(newX, newY);
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
     return (
-        <div style={{ top: y, left: x }} className={className ? `block ${className}` : 'block'}>
+        <div
+            style={{ top: y, left: x }}
+            className={className ? `block ${className}` : 'block'}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+        >
             {children}
         </div>
     );
